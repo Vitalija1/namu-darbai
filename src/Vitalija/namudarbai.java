ea4b43045254ffba77b9b7798dab9d0fa5e90dd2
Vitalija1 <vitalija.lekaviciene@gmail.com>
2019-10-11 13:50:05 +0300

    first commit

---
+++ src/Vitalija/namudarbai.java
@@ -0,0 +1,10 @@
+package Vitalija;
+
+public class namudarbai {
+    public static void main(String[] args) {
+        String vardas = "Vitalija";
+        String pavarde = "Lekaviciene";
+        String pareigos = "Business Analyst";
+        System.out.println("Hello, my name is " + vardas + " " + pavarde + ". I am " + pareigos + ". ");
+    }
+    }
